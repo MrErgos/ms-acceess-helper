09fe54e9d03b83209b9689c68633b587415e09f2
George <babin.svet.v@gmail.com>
2025-10-21 16:59:37 +0500

    Added filter and paginating functionality and buttons on main page Created header

--- manager-app/src/main/java/io/github/mrergos/controller/MemberController.java
+++ manager-app/src/main/java/io/github/mrergos/controller/MemberController.java
@@ -1,22 +1,40 @@
 package io.github.mrergos.controller;
 
 import io.github.mrergos.client.MembersRestClient;
+import io.github.mrergos.entity.MemberNkso;
+import io.github.mrergos.util.Pair;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.*;
+import java.util.stream.IntStream;
+
+@Slf4j
 @Controller
-@RequestMapping(path = "members/")
+@RequestMapping(path = "members")
 @RequiredArgsConstructor
 public class MemberController {
     private final MembersRestClient membersRestClient;
 
     @GetMapping
-    public String getMemberList(Model model) {
-        model.addAttribute("members", membersRestClient.findAllMembers());
+    public String getMemberList(@RequestParam(value = "filter", required = false, defaultValue = "") String filter,
+                                @RequestParam(value = "page", defaultValue = "0") int page,
+                                Model model) {
+        model.addAttribute("filter", filter);
+        Pair totalPagesAndMembers = membersRestClient.getMembersPageByFilter(filter, page);
+        int totalPages = totalPagesAndMembers.getFirst();
+        model.addAttribute("totalPages", totalPages);
+        model.addAttribute("members", totalPagesAndMembers.getSecond());
+        if (totalPages > 1) {
+            Set<Integer> pageNumbers = new HashSet<>();
+            IntStream.range(1, totalPages-1)
+                    .takeWhile(pageNumber -> ((pageNumber > page - 3) && (pageNumber < page + 3)))
+                    .forEach(pageNumber -> pageNumbers.add(pageNumber+1));
+            model.addAttribute("pageNumbers", pageNumbers);
+        }
         return "members";
     }
 
