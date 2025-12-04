a3ab9965b6ad0dbc67e7e44aec80676a91f842ec
George <mrergos.babin@gmail.com>
2025-12-05 02:50:19 +0500

    feat(manager-app): add full logout

--- manager-app/src/main/java/io/github/mrergos/config/SecurityBeans.java
+++ manager-app/src/main/java/io/github/mrergos/config/SecurityBeans.java
@@ -1,5 +1,6 @@
 package io.github.mrergos.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
@@ -8,10 +9,13 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserRequest;
 import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserService;
+import org.springframework.security.oauth2.client.oidc.web.logout.OidcClientInitiatedLogoutSuccessHandler;
+import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
 import org.springframework.security.oauth2.core.oidc.user.DefaultOidcUser;
 import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 
 import java.util.List;
 import java.util.Optional;
@@ -19,16 +23,31 @@ import java.util.stream.Stream;
 
 @Configuration
 public class SecurityBeans {
+    @Autowired
+    private InMemoryClientRegistrationRepository clientRegistrationRepository;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/members/{registryNum}/edit").hasRole("EDITOR")
+                        .requestMatchers("/members/create").hasRole("EDITOR")
                         .anyRequest().permitAll())
                 .oauth2Login(Customizer.withDefaults())
+                .logout(logout -> logout
+                        .clearAuthentication(true)
+                        .invalidateHttpSession(true)
+                        .deleteCookies("JSESSIONID")
+                        .logoutSuccessHandler(oidcLogoutSuccessHandler()))
                 .oauth2Client(Customizer.withDefaults())
                 .build();
+    }
+
+    private LogoutSuccessHandler oidcLogoutSuccessHandler() {
+        OidcClientInitiatedLogoutSuccessHandler oidcClientInitiatedLogoutSuccessHandler =
+                new OidcClientInitiatedLogoutSuccessHandler(this.clientRegistrationRepository);
+        oidcClientInitiatedLogoutSuccessHandler.setPostLogoutRedirectUri("http://localhost:8080/members");
+        return oidcClientInitiatedLogoutSuccessHandler;
     }
 
     @Bean
