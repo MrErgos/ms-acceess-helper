a1034ec98b83a99898660eabd7e363b129c76a03
George <mrergos.babin@gmail.com>
2025-12-10 00:10:55 +0500

    fix(manager-nkso-api): fix secure endpoints

--- member-nkso-api/src/main/java/io/github/mrergos/config/SecurityConfig.java
+++ member-nkso-api/src/main/java/io/github/mrergos/config/SecurityConfig.java
@@ -7,10 +7,6 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -22,8 +18,8 @@ public class SecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(HttpMethod.POST, "/member-nkso-api/members").hasAuthority("SCOPE_edit_members")
-                        .requestMatchers(HttpMethod.DELETE, "/member-nkso-api/members").hasRole("SCOPE_edit_members")
-                        .requestMatchers(HttpMethod.PUT, "/member-nkso-api/members").hasRole("SCOPE_edit_members")
+                        .requestMatchers(HttpMethod.DELETE, "/member-nkso-api/members/**").hasAuthority("SCOPE_edit_members")
+                        .requestMatchers(HttpMethod.PUT, "/member-nkso-api/members/**").hasAuthority("SCOPE_edit_members")
                         .anyRequest().permitAll())
                 .oauth2ResourceServer(oauth2ResourceServer -> oauth2ResourceServer
                         .jwt(Customizer.withDefaults()))
